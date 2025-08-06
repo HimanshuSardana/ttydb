8bee15af04353d1abe971b51e0946f5a3de8b429
Himanshu Sardana <himanshusardana2005@gmail.com>
2025-08-06 12:53:39 +0530

    feat: generator now writes to csv file

--- scraper/main.py
+++ scraper/main.py
@@ -1,4 +1,5 @@
 from google import genai
+import csv
 from src.generator import Generator
 
 topics = ["Basic Selection", "Filtering (WHERE clause)", "Joins", "Aggregation (GROUP BY)", "Window Functions", "Subqueries", "Set Operations", "Date and Time Functions"]
@@ -7,9 +8,15 @@ def main():
     for topic in topics:
         generator = Generator(topic)
         data = generator.generate_data(num_rows=10)
-        print(f"Generated data for topic '{topic}':")
         print(data)
-        break
+
+        with open(f"data.csv", mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Instruction", "Query", "Table Schema", "Explanation"])
+            for item in data:
+                writer.writerow([item['instruction'], item['query'], item['table_schema'], item['explanation']])
+            print(f"10 rows for topic '{topic}' written to data.csv")
+
 
 if __name__ == "__main__":
     main()
