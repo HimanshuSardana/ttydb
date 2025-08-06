0622bfd3b759a7dd9a6336db854430448f1291a2
Himanshu Sardana <himanshusardana2005@gmail.com>
2025-08-06 13:46:27 +0530

    feat: add 2k rows synthetic data

--- scraper/main.py
+++ scraper/main.py
@@ -1,22 +1,26 @@
 from google import genai
 import csv
+import logging
 from src.generator import Generator
+
+# print nothing except these logs
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] - %(levelname)s - %(message)s')
 
 topics = ["Basic Selection", "Filtering (WHERE clause)", "Joins", "Aggregation (GROUP BY)", "Window Functions", "Subqueries", "Set Operations", "Date and Time Functions"]
 
 def main():
+    rows = 50
     for topic in topics:
         generator = Generator(topic)
-        data = generator.generate_data(num_rows=10)
-        print(data)
+        data = generator.generate_data(num_rows=rows)
+        # print(data)
 
-        with open(f"data.csv", mode='w', newline='') as file:
+        with open(f"data.csv", mode='a', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(["Instruction", "Query", "Table Schema", "Explanation"])
             for item in data:
                 writer.writerow([item['instruction'], item['query'], item['table_schema'], item['explanation']])
-            print(f"10 rows for topic '{topic}' written to data.csv")
-
+            logging.info(f"{rows} rows for topic '{topic}' written to data.csv")
 
 if __name__ == "__main__":
     main()
--- scraper/src/generator/__init__.py
+++ scraper/src/generator/__init__.py
@@ -3,16 +3,17 @@ from dotenv import load_dotenv
 from google.genai import types
 from pydantic import BaseModel, Field
 import os
+import json
 
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 class Query(BaseModel):
-    instruction: str = Field(..., description="The natural language instruction for the query.")
-    query: str = Field(..., description="The query corresponding to the natural language request.")
-    table_schema: str = Field(..., description="The schema of the table used in the query.")
-    explanation: str = Field(..., description="The explanation of the query.")
+    instruction: str = Field(..., description="The natural language instruction for the query. Do not enclose in quotes.")
+    query: str = Field(..., description="The query corresponding to the natural language request. Do not enclose in quotes.")
+    table_schema: str = Field(..., description="The schema of the table used in the query. Do not enclose in quotes.")
+    explanation: str = Field(..., description="The explanation of the query. Do not enclose in quotes.")
 
 class Generator:
     def __init__(self, topic):
@@ -32,4 +33,4 @@ class Generator:
                 'response_schema': list[Query],
             }
         )
-        return response.text
+        return json.loads(response.text)
